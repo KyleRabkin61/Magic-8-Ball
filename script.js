9f46290d521afeb6c8497b19907aa8eb00431835
KyleRabkin61 <kylerabkin6161@gmail.com>
2023-12-14 10:42:56 -0500

    added starter javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+function shakeMagic8Ball(){
+    const question = prompt('Ask the Magic 8-Ball a question:')
+    if (question === null) {
+        document.getElementById('response-text').innerText = 'Looks like you did not enter a question, please ask a question.'
+        document.getElementById('response-text').classList = 'display text-danger'
+        document.getElementById('response-image').src = 'bad image'
+    }
+    if(!question.trim()) {
+        alert('Please enter a valid question!')
+        return
+    }
+}
