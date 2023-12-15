53ee380e0d4a87d47aeaadcb5169ec0ab79a7703
KyleRabkin61 <kylerabkin6161@gmail.com>
2023-12-15 11:37:00 -0500

    finished javascript

--- script.js
+++ script.js
@@ -20,37 +20,37 @@ function shakeMagic8Ball() {
             color = "text-success";
             break;
         case 1:
-            eightBall = 'It is decidedly so';
+            answer = 'It is decidedly so';
             image = "yes-image.jpg";
             color = "text-success";
             break;
         case 2:
-            eightBall = 'Reply hazy try again';
+            answer = 'Reply hazy try again';
             image = "no-image.jpg";
             color = "text-danger";
             break;
         case 3:
-            eightBall = 'Cannot predict now';
+            answer = 'Cannot predict now';
             image = "no-image.jpg";
             color = "text-danger";
             break;
         case 4:
-            eightBall = 'Do not count on it';
+            answer = 'Do not count on it';
             image = "no-image.jpg";
             color = "text-danger";
             break;
         case 5:
-            eightBall = 'My sources say no';
+            answer = 'My sources say no';
             image = "no-image.jpg";
             color = "text-danger";
             break;
         case 6:
-            eightBall = 'Outlook not so good';
+            answer = 'Outlook not so good';
             image = "no-image.jpg";
             color = "text-danger";
             break;
         case 7:
-            eightBall = 'Signs point to yes';
+            answer = 'Signs point to yes';
             image = "yes-image.jpg";
             color = "text-success";
             break;
